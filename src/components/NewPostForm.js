49675b69d9e0b27e9dac3e517cdfcd12049f0586
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-27 14:46:17 -0700

    Create reusable and new post form components.
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,1 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types'
+import ReusableForm from './ReusableForm';
+
+function newPostForm(props) {
+  return(
+    <>
+      <ReusableForm formSubmissionHandler={handleNewPostSubmissionForm}
+      buttonText='New Post' />
+    </>
+  );
+
+  function handleNewPostSubmissionForm(event) {
+    event.preventDefault();
+    props.onNewPostCreation({author: event.target.author.value,
+    title: event.target.title.value,
+    content: event.target.content.value,
+    votes: 0,
+    timeStamp: `${(new Date()).getMonth()}/${(new Date()).getDate()}/${(new Date()).getFullYear()}`,
+    id: v4()});
+  }
+}
+
+NewPostForm.PropTypes = {
+  onNewPostCreation: PropTypes.func
+};
+
+export default newPostForm;
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,1 +1,30 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='author'
+          placeholder='Author' />
+        <input
+          type='text'
+          name='title'
+          placeholder='Title' />
+        <textarea
+          name='content'
+          placeholder='Enter your post here!' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
