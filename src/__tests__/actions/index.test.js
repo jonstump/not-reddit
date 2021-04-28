9307fd6ac1d019fe8f3e09e57b2efad2d0f46346
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-28 15:08:10 -0700

    Create and test add post action creator.
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -7,4 +7,25 @@ describe('help queue actions', () => {
       id: 1
     });
   });
+
+
+  it('addPost should create ADD_POST action', () => {
+    const clock = `${(new Date()).getMonth()}/${(new Date()).getDate()}/${(new Date()).getFullYear()}`
+    expect(actions.addPost({
+      author: 'Mac Daddy',
+      title: 'title',
+      content: 'content',
+      votes: 0,
+      timeStamp: clock,
+      id: 1
+    })).toEqual({
+      type: 'ADD_POST',
+      author: 'Mac Daddy',
+      title: 'title',
+      content: 'content',
+      votes: 0,
+      timeStamp: clock,
+      id: 1
+    })
+  });
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,16 @@ export const deletePost = id => ({
   type: 'DELETE_POST',
   id
 });
+
+export const addPost = (post) => {
+  const { title, content, author, votes, timeStamp, id } = post;
+  return {
+  type: 'ADD_POST',
+  title: title,
+  content: content,
+  author: author,
+  votes: votes,
+  timeStamp: timeStamp,
+  id: id
+  }
+}
